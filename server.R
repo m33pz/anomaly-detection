3254916a1ec6f7f8c10dc987941769322c379976
m33pz <jeclark@rubiksgeek.com>
2014-07-08 23:25:58 -0700

    updated gpl

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-#<anomaly detection system by m33p>
+#<anomaly detection system>
 #Copyright (C) <2014> <m33p>
 #
 #This program is free software: you can redistribute it and/or modify
@@ -13,7 +13,7 @@
 #
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+#contact m33p at m33p@rubiksgeek.com for more information on usage.
 
 library(shiny)
 
